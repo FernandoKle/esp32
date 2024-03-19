782052f49004e2878b0597dec6293c797e8427eb
Fernando Kleinubing <kleinubing84@gmail.com>
2024-03-19 16:27:33 -0300

    conecta al wifi

--- boot.py
+++ boot.py
@@ -1,9 +1,10 @@
 import network
+from my_config import SSID, PASS
 
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 if not wlan.isconnected():
     print('conectando a la red...')
-    wlan.connect('SSID', 'PASSWORD')
+    wlan.connect(SSID, PASS)
     while not wlan.isconnected():
         pass
