02cf1cd05a175e94578b864a32ff88d63d9fc344
Fernando Kleinubing <kleinubing84@gmail.com>
2024-03-19 15:47:11 -0300

    anti rebote

--- main.py
+++ main.py
@@ -8,10 +8,16 @@ print("esperando pulsador")
 sw = Pin(23, Pin.IN)
 led = Pin(2, Pin.OUT)
 contador = 0
+sw_flag = True
 
 while True:
-    if sw.value():
+    if sw.value() and sw_flag:
         led.value(not led.value())
+        sw_flag = False
         contador += 1
         print(contador)
+    
+    if sw.value() == False and sw_flag == False:
+        sw_flag = True
+    
     time.sleep_ms(5)
